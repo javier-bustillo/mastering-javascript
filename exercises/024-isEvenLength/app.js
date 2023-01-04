e1fb88e94742a511119a50351424e8e2a6a31949
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 19:18:29 +0000

    exercise 24 passed

--- exercises/024-isEvenLength/app.js
+++ exercises/024-isEvenLength/app.js
@@ -1,1 +1,8 @@
-// Write your function here
+const isEvenLength = word => {
+    let wordLength = word.length;
+
+    if (wordLength % 2 === 0) return true;
+    else return false;
+}
+let output = isEvenLength('Root');
+console.log(output);
