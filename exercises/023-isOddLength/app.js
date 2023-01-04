3cfb758314ab2c655154841e1d58e62a3e85b83c
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 19:15:32 +0000

    exercise 23 passed

--- exercises/023-isOddLength/app.js
+++ exercises/023-isOddLength/app.js
@@ -1,4 +1,9 @@
-// Write your function here
-function isOddLength(word){
-    
+const isOddLength = word => {
+    let wordLength = word.length;
+
+    if (wordLength % 2 !== 0) return true;
+    else return false;
+
 }
+let output = isOddLength('specimen');
+console.log(output);
