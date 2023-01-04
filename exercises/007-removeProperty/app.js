63a9c35a5965ca4ef517a830a0f00eecb6446934
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 18:18:17 +0000

    exercise 7 passed

--- exercises/007-removeProperty/app.js
+++ exercises/007-removeProperty/app.js
@@ -1,3 +1,12 @@
-function removeProperty(obj, key) {
-  // your code here
+let myObj = {
+    name: 'Sam',
+    age: 20
 }
+
+const removeProperty = (obj, key) => {
+    delete obj[key];
+    return obj;
+}
+
+removeProperty(myObj, 'name');
+console.log(myObj.name);
