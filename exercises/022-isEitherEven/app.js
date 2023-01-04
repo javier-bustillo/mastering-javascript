4287c8d2f39bf79076e589682f03e268e0103415
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 19:10:54 +0000

    exercise 22 passed

--- exercises/022-isEitherEven/app.js
+++ exercises/022-isEitherEven/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+const isEitherEven = (num1, num2) => {
+    if (num1 % 2 === 0 || num2 % 2 === 0) return true;
+    else return false;
+}
+let output = isEitherEven(5, 20);
+console.log(output);
