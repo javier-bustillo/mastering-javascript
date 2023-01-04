628eaf40c1fd500f044fa5cad0903c157fd93412
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 19:06:58 +0000

    exercise 21 passed

--- exercises/021-areBothOdd/app.js
+++ exercises/021-areBothOdd/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+const areBothOdd = (num1, num2) => {
+    if ((num1 % 2 !== 0) && (num2 % 2 !== 0)) return true;
+    else return false;
+}
+let output = areBothOdd(10, 11);
+console.log(output);
