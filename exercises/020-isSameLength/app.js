286cb60ac61ba934c50719e2790d2e4ba9c02b27
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 19:01:56 +0000

    exercise 20 passed

--- exercises/020-isSameLength/app.js
+++ exercises/020-isSameLength/app.js
@@ -1,1 +1,9 @@
-// Write your function here
+const isSameLength = (word1, word2) => {
+    let word1Length = word1.length;
+    let word2Length = word2.length;
+
+    if (word1Length === word2Length) return true;
+    else return false;
+}
+let output = isSameLength('He', 'lo');
+console.log(output);
