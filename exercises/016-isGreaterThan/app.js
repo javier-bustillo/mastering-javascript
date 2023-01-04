8c8101ca27fc653c8d6d2c07d0003de82680ca1f
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 18:49:52 +0000

    exercise 16 passed

--- exercises/016-isGreaterThan/app.js
+++ exercises/016-isGreaterThan/app.js
@@ -1,3 +1,7 @@
-function isGreaterThan(num1, num2) {
-  // your code here
+const isGreaterThan = (num1, num2) => {
+    if (num2 > num1) return true;
+    else return false;
 }
+
+let output = isGreaterThan(10, 20);
+console.log(output);
