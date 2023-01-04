9a20b6bb7279f220492d9805fe261d9c0ddcf3a5
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 18:40:48 +0000

    exercise 13 passed

--- exercises/013-isLessThanThirty/app.js
+++ exercises/013-isLessThanThirty/app.js
@@ -1,3 +1,6 @@
-function isLessThan30(num) {
-  // your code here
+const isLessThan30 = num => {
+    if (num < 30) return true;
+    else return false;
 }
+let output = isLessThan30(12);
+console.log(output);
