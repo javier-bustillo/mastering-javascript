eea834f497e92f6db3d1189396b0f3d3aa2e649c
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 18:52:39 +0000

    exercise 17 passed

--- exercises/017-isEqualTo/app.js
+++ exercises/017-isEqualTo/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+const isEqualTo = (num1, num2) => {
+    if (num2 === num1) return true;
+    else return false;
+}
+let output = isEqualTo(19, 10);
+console.log(output);
