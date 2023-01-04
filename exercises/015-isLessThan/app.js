696061510e636d313aad3d03624cf999ed760aac
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 18:47:44 +0000

    exercise 15 passed

--- exercises/015-isLessThan/app.js
+++ exercises/015-isLessThan/app.js
@@ -1,3 +1,6 @@
-function isLessThan(num1, num2) {
-  // your code here
+const isLessThan = (num1, num2) => {
+    if (num2 < num1) return true;
+    else return false
 }
+let output = isLessThan(40, 20);
+console.log(output);
