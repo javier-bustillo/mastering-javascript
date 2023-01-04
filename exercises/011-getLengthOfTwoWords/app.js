46ac7a21b4e8410f3f2e2f9357d7fd1ba636ac4e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-04 18:34:41 +0000

    exercise 11 passed

--- exercises/011-getLengthOfTwoWords/app.js
+++ exercises/011-getLengthOfTwoWords/app.js
@@ -1,3 +1,9 @@
-function getLengthOfTwoWords(word1, word2) {
-  // your code here
+const getLengthOfTwoWords = (word1, word2) => {
+    let word1Length = word1.length;
+    let word2Length = word2.length;
+    let sum = word1Length + word2Length;
+
+    return sum;
 }
+let output = getLengthOfTwoWords('Hello', 'Ninjas');
+console.log(output);
