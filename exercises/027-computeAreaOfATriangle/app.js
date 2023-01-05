e9f7ebca92f28a448fc5faab4e3872e52f9cd806
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 17:46:31 +0000

    exercise 27 passed

--- exercises/027-computeAreaOfATriangle/app.js
+++ exercises/027-computeAreaOfATriangle/app.js
@@ -1,2 +1,7 @@
-// Write your function here
+const computeAreaOfATriangle = (base, height) => {
+    let triangleArea = 1 / 2 * base * height;
+    return triangleArea;
+}
 
+let output = computeAreaOfATriangle(4, 6);
+console.log(output);
