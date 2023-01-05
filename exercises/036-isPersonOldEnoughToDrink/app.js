ddda4396845ae027cba57d96c2b2fb9fd48c2b62
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 19:16:34 +0000

    exercise 36 passed

--- exercises/036-isPersonOldEnoughToDrink/app.js
+++ exercises/036-isPersonOldEnoughToDrink/app.js
@@ -1,4 +1,11 @@
-function isPersonOldEnoughToDrink(person) {
-  // Add your code after this line
-  
+let person = {
+    age: 21
 }
+
+const isPersonOldEnoughToDrink = person => {
+    if (person.age >= 21) return true;
+    else return false;
+
+}
+let output = isPersonOldEnoughToDrink(person);
+console.log(output);
