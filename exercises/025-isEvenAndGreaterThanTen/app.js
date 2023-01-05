aa40af80ff3b1ed10ffe843a00b43eb695e482a8
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 17:39:33 +0000

    exercise 25 passed

--- exercises/025-isEvenAndGreaterThanTen/app.js
+++ exercises/025-isEvenAndGreaterThanTen/app.js
@@ -1,1 +1,7 @@
-// Write your function here
+const isEvenAndGreaterThanTen = num => {
+    if (num % 2 === 0 && num > 10) return true;
+    else return false;
+}
+
+let output = isEvenAndGreaterThanTen(9);
+console.log(output);
