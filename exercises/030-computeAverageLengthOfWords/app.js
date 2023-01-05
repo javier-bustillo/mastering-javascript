2e4a7176783ca230afe87e392b6ebdb145b6a3f8
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 17:59:02 +0000

    exercise 30 passed

--- exercises/030-computeAverageLengthOfWords/app.js
+++ exercises/030-computeAverageLengthOfWords/app.js
@@ -1,1 +1,11 @@
-// Write your function here
+const computeAverageLengthOfWords = (word1, word2) => {
+    let word1Length = word1.length;
+    let word2Length = word2.length;
+    let totalLength = word1Length + word2Length;
+
+    return totalLength / 2;
+
+}
+
+let output = computeAverageLengthOfWords('code', 'programs');
+console.log(output);
