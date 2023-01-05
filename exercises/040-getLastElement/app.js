ce61370986012e4e27348f77a233b5fadd0383cb
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 19:51:34 +0000

    exercise 40 passed

--- exercises/040-getLastElement/app.js
+++ exercises/040-getLastElement/app.js
@@ -1,4 +1,7 @@
-function getLastElement(array) {
-  // Add your code after this line
-  return 4
+const getLastElement = array => {
+    if (array.length === 0) return undefined;
+    else return array[array.length - 1];
 }
+
+let output = getLastElement([0, 1, 0, 2]);
+console.log(output);
