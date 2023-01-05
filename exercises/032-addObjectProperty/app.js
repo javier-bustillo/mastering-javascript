1e18e0e9a77472d333862061f8c1dd405e4f5b55
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 18:48:06 +0000

    exercise 32 passed

--- exercises/032-addObjectProperty/app.js
+++ exercises/032-addObjectProperty/app.js
@@ -1,4 +1,17 @@
-function addObjectProperty(obj1, key, obj2) {
-  // Add your code after this line
-
+let person1 = {
+    name: 'Joe Blow',
+    role: 'schlub'
 }
+
+let person2 = {
+    name: 'Mr. Burns',
+    role: 'supervisor'
+}
+
+const addObjectProperty = (myObj1, key, myObj2) => {
+    myObj1[key] = myObj2;
+    return myObj1;
+}
+
+addObjectProperty(person1, 'manager', person2);
+console.log(person1.manager);
