213f953c1ec1307e1a85d49737091d7975ae72ad
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-05 19:04:50 +0000

    exercise 33 passed

--- exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
+++ exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
@@ -1,5 +1,13 @@
-function isPersonOldEnoughToDrinkAndDrive(person) {
-  // the person object contains an "age" property inside
-  // Add your code after this line
-  
+let person = {
+    age: 20
 }
+
+const isPersonOldEnoughToDrinkAndDrive = myObj => {
+    // the person object contains an "age" property inside
+    // Add your code after this line
+
+    if (myObj.age >= 21 || myObj.age >= 23) return true;
+    else if (myObj.age < 21) return false;
+}
+let output = isPersonOldEnoughToDrinkAndDrive(person);
+console.log(output);
