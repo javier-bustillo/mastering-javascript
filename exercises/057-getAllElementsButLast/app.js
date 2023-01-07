8cbbe429e7ec40b8a3f01da7a93e6cded51837f8
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 17:18:01 +0000

    exercise 57 passed

--- exercises/057-getAllElementsButLast/app.js
+++ exercises/057-getAllElementsButLast/app.js
@@ -1,4 +1,8 @@
-function getAllElementsButLast(array) {
-    // your code here
+const getAllElementsButLast = array => {
+    const newArr = array.slice(0, -1);
+    return newArr;
 
 }
+let input = [1, 2, 3, 4];
+let output = getAllElementsButLast(input);
+console.log(output);
