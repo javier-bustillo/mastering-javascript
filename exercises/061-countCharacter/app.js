37c9ae25a3e63862849f84bb68036e31844ec6e5
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 17:52:40 +0000

    exercise 61 passed

--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,4 +1,14 @@
-function countCharacter(str, char) {
-    // your code here
-    
+const countCharacter = (str, char) => {
+    let i = 0;
+    let aux = 0;
+    for (i; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            aux += 1;
+        };
+
+    }
+
+    return aux;
 }
+let output = countCharacter('I am a hacker', 'a');
+console.log(output);
