e081e8549f20a12c4287893a930bb7c90639017f
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 17:07:44 +0000

    exercise 55 passed

--- exercises/055-getElementsUpTo/app.js
+++ exercises/055-getElementsUpTo/app.js
@@ -1,4 +1,7 @@
-function getElementsUpTo(array, n) {
-  // your code here
-  
+const getElementsUpTo = (array, n) => {
+    const newArr = array.slice(0, n);
+    return newArr;
+
 }
+let output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3);
+console.log(output);
