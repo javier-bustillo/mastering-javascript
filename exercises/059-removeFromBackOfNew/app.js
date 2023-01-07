bdcd49bffeb316b2754a1fc1f8a86168e28cc2a7
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 17:32:47 +0000

    exercise 59 passed

--- exercises/059-removeFromBackOfNew/app.js
+++ exercises/059-removeFromBackOfNew/app.js
@@ -1,4 +1,8 @@
-function removeFromBackOfNew(arr) {
-  // your code here
-  
+const removeFromBackOfNew = arr => {
+    const newArr = arr.slice(0, -1);
+    return newArr;
 }
+let arr = [1, 2, 3];
+let output = removeFromBackOfNew(arr);
+console.log(output);
+console.log(arr);
