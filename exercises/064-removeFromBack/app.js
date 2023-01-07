5c95f0fa4c9ded0f7792d4a2c59b3cfd03a6fd05
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 18:35:07 +0000

    exercise 64 passed

--- exercises/064-removeFromBack/app.js
+++ exercises/064-removeFromBack/app.js
@@ -1,4 +1,7 @@
-function removeFromBack(arr) {
-    // your code here
-    
+const removeFromBack = arr => {
+    arr.pop();
+    return arr;
+
 }
+let output = removeFromBack([1, 2, 3]);
+console.log(output);
