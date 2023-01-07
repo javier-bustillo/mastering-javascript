edbcfbe476e53878efa6cb25f12e544b8a4d0ef6
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 16:25:26 +0000

    exercise 45 passed

--- exercises/045-computePerimeterOfATriangle/app.js
+++ exercises/045-computePerimeterOfATriangle/app.js
@@ -1,3 +1,6 @@
-function computePerimeterOfATriangle(side1, side2, side3) {
-  // your code here
+const computePerimeterOfATriangle = (side1, side2, side3) => {
+    let result = side1 + side2 + side3;
+    return result;
 }
+let output = computePerimeterOfATriangle(6, 4, 10);
+console.log(output);
