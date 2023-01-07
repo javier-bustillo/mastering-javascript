740aa4410c743727a666fabb00bcbc715ed761e6
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 16:45:35 +0000

    exercise 52 passed

--- exercises/052-getLengthOfThreeWords/app.js
+++ exercises/052-getLengthOfThreeWords/app.js
@@ -1,3 +1,6 @@
-function getLengthOfThreeWords(word1, word2, word3) {
-  // your code here
+const getLengthOfThreeWords = (word1, word2, word3) => {
+    let result = word1.length + word2.length + word3.length;
+    return result;
 }
+let output = getLengthOfThreeWords('some', 'other', 'words');
+console.log(output);
