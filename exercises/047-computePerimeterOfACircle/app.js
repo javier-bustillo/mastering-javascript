0af913c80026e7a28c83e7f7d67b8446ab3682d5
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 16:32:21 +0000

    exercise 47 passed

--- exercises/047-computePerimeterOfACircle/app.js
+++ exercises/047-computePerimeterOfACircle/app.js
@@ -1,4 +1,8 @@
-function computePerimeterOfACircle(radius) {
-  // your code here
+const computePerimeterOfACircle = radius => {
+    let result = 2 * Math.PI * radius;
+    return result;
 
 }
+
+let output = computePerimeterOfACircle(4);
+console.log(output);
