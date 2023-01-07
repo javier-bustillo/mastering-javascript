d98f309db6503d5b96607948811688234f04815e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 16:43:16 +0000

    exercise 51 passed

--- exercises/051-doubleSquareRootOf/app.js
+++ exercises/051-doubleSquareRootOf/app.js
@@ -1,4 +1,7 @@
-function doubleSquareRootOf(num) {
-  // your code here
-  
+const doubleSquareRootOf = num => {
+    let result = Math.sqrt(num) * 2;
+    return result;
+
 }
+let output = doubleSquareRootOf(121);
+console.log(output);
