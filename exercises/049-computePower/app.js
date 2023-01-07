e8a4ca6edf3fca0c429bf95ad1cafc28e22535cf
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 16:39:16 +0000

    exercise 49 passed

--- exercises/049-computePower/app.js
+++ exercises/049-computePower/app.js
@@ -1,3 +1,7 @@
-function computePower(num, exponent) {
-  // your code here
+const computePower = (num, exponent) => {
+    let result = Math.pow(num, exponent);
+    return result;
 }
+
+let output = computePower(2, 3);
+console.log(output);
