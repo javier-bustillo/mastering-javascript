d7d09dfc3b3b02917e6fc0f5ba34d069a3e0afa7
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 16:37:11 +0000

    exercise 48 passed

--- exercises/048-computeAreaOfACircle/app.js
+++ exercises/048-computeAreaOfACircle/app.js
@@ -1,3 +1,7 @@
-function computeAreaOfACircle(radius) {
-    // your code here
+const computeAreaOfACircle = radius => {
+    let result = Math.PI * Math.pow(radius, 2);
+    return result;
 }
+
+let output = computeAreaOfACircle(4);
+console.log(output);
