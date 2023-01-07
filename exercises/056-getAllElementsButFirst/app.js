3d4966186400594d3aa8a7553082998b220311a2
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-07 17:11:16 +0000

    exercise 56 passed

--- exercises/056-getAllElementsButFirst/app.js
+++ exercises/056-getAllElementsButFirst/app.js
@@ -1,4 +1,8 @@
-function getAllElementsButFirst(array) {
-  // your code here
-  
+const getAllElementsButFirst = array => {
+    const newArr = array.slice(1);
+    return newArr;
+
 }
+let input = [1, 2, 3, 4];
+let output = getAllElementsButFirst(input);
+console.log(output);
