b72d215f4d55e2ac052aa25e25ca2ec9c18a0b9b
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 19:18:56 +0000

    exercise 72 passed

--- exercises/072-removeEvenValues/app.js
+++ exercises/072-removeEvenValues/app.js
@@ -1,4 +1,16 @@
-function removeEvenValues(obj) {
-    // your code here
-    
+let obj = {
+    a: 2,
+    b: 3,
+    c: 4
+};
+
+const removeEvenValues = obj => {
+    for (let key in obj) {
+        if (obj[key] % 2 === 0) delete obj[key];
+    }
+
+    return obj;
 }
+
+removeEvenValues(obj);
+console.log(obj);
