e8debc711201df870a776aa6967ab49e165a4353
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 18:59:57 +0000

    exercise 68 passed

--- exercises/068-extend/app.js
+++ exercises/068-extend/app.js
@@ -7,7 +7,18 @@ let obj2 = {
     c: 3
 };
 
-function extend(obj1, obj2) {
-    // your code here
-
+const extend = (obj1, obj2) => {
+    for (let key in obj2) {
+        if (obj1[key]) {
+            obj1[key];
+        } else {
+            obj1[key] = obj2[key];
+        }
+    }
+    return obj1;
 }
+
+extend(obj1, obj2);
+
+console.log(obj1);
+console.log(obj2);
