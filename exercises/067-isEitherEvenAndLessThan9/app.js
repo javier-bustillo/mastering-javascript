8e225c5f385bbe24c47064782f9866b3df401ec6
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 17:44:10 +0000

    exercise 67 passed

--- exercises/067-isEitherEvenAndLessThan9/app.js
+++ exercises/067-isEitherEvenAndLessThan9/app.js
@@ -1,4 +1,12 @@
-function isEitherEvenAndLessThan9(num1, num2) {
-    // your code here
+const isEitherEvenAndLessThan9 = (num1, num2) => {
+    if ((num1 % 2 === 0 || num2 % 2 === 0) && (num1 < 9 && num2 < 9))
+        return true;
+    else return false;
 
 }
+
+let output = isEitherEvenAndLessThan9(2, 4);
+console.log(output);
+
+let output2 = isEitherEvenAndLessThan9(72, 2);
+console.log(output2);
