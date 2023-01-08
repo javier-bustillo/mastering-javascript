f95cbd51daf0ae8d199cc761cc2b032af47939f4
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 20:00:16 +0000

    exercise 79 passed

--- exercises/079-joinThreeArrays/app.js
+++ exercises/079-joinThreeArrays/app.js
@@ -1,4 +1,9 @@
-function joinThreeArrays(arr1, arr2, arr3) {
-  // your code here
-  
+const joinThreeArrays = (arr1, arr2, arr3) => {
+    // let result = [...arr1, ...arr2, ...arr3];
+    let result = arr1.concat(arr2, arr3);
+    return result;
+
 }
+
+let output = joinThreeArrays([1, 2], [3, 4], [5, 6]);
+console.log(output);
