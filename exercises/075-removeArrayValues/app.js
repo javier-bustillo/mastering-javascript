994c73fb1bba39951b4a60e64956c8b8270f4f6b
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 19:33:00 +0000

    exercise 75 passed

--- exercises/075-removeArrayValues/app.js
+++ exercises/075-removeArrayValues/app.js
@@ -1,4 +1,17 @@
-function removeArrayValues(obj) {
-    // your code here
-    
+let obj = {
+    a: [1, 2, 3],
+    b: 2,
+    c: ['hi', 'there']
 }
+
+const removeArrayValues = obj => {
+    for (let key in obj) {
+        if (Array.isArray(obj[key])) {
+            delete obj[key];
+        }
+    }
+    return obj;
+}
+
+removeArrayValues(obj);
+console.log(obj);
