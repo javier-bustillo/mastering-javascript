0c7578f255672e82122d24ff0d33f8baf2eef349
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 19:23:45 +0000

    exercise 73 passed

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,14 +1,19 @@
 let obj = {
-    person:"Rafael",
-    lastName:"Leao",
+    person: "Rafael",
+    lastName: "Leao",
     country: "Italy",
     city: "Milan",
     phoneNum: "123-456-789",
     jerseyNum: 27,
 }
 
-function countNumberOfKeys(obj) {
-    // your code here
-   
+const countNumberOfKeys = obj => {
+    let aux = 0;
+    for (let key in obj) {
+        if (obj[key]) {
+            aux += 1;
+        }
+    }
+    return aux;
 }
 console.log(countNumberOfKeys(obj))
