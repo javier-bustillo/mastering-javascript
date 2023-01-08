7d424f3fc45426d22a9856351cbf92c7578c1311
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-08 19:27:16 +0000

    exercise 74 passed

--- exercises/074-removeOddValues/app.js
+++ exercises/074-removeOddValues/app.js
@@ -1,4 +1,17 @@
-function removeOddValues(obj) {
-    // your code here
+let obj = {
+    a: 2,
+    b: 3,
+    c: 4
+};
+const removeOddValues = obj => {
+    for (let key in obj) {
+        if (obj[key] % 2 !== 0) {
+            delete obj[key];
+        }
+    }
+    return obj;
 
 }
+
+removeOddValues(obj);
+console.log(obj)
