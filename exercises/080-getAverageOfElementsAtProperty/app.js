34d2d986d50a75591999b0c12681741e5491558a
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-09 10:18:56 +0000

    exercise 80 passed

--- exercises/080-getAverageOfElementsAtProperty/app.js
+++ exercises/080-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,19 @@
-// Write your function here
+let obj = {
+    key: [1, 2, 3]
+};
+
+const getAverageOfElementsAtProperty = (obj, key) => {
+    let sum = 0;
+    let average = 0;
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return 0;
+    else
+        for (let i in obj[key]) {
+            sum += obj[key][i];
+        }
+    average = sum / obj[key].length;
+    return average;
+}
+
+let output = getAverageOfElementsAtProperty(obj, 'key');
+console.log(output);
