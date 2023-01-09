4b97d28eceac33c0251e0b685137417facd0837a
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-09 10:54:23 +0000

    exercise 81 passed

--- exercises/081-addToFrontOfNew/app.js
+++ exercises/081-addToFrontOfNew/app.js
@@ -1,9 +1,11 @@
-function addToFrontOfNew(arr, element) {
-    // Write your function here
-    
+const addToFrontOfNew = (arr, element) => {
+    const newArr = arr.slice(0);
+    newArr.unshift(element);
+    return newArr;
 }
+
 
 let input = [1, 2];
 let output = addToFrontOfNew(input, 3);
 console.log(output); // --> [3, 1, 2];
-console.log(input); // --> [1, 2]
+console.log(input); // --> [1, 2], 2];
