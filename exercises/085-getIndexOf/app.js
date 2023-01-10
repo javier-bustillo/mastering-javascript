e5ba916534ba5d4a1d2de07b6e0f5f0ba6c23282
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-10 09:27:06 +0000

    exercise 85 passed

--- exercises/085-getIndexOf/app.js
+++ exercises/085-getIndexOf/app.js
@@ -1,5 +1,9 @@
 const getIndexOf = (char, str) => {
-
+    for (let i = 0; i < str.length; i++) {
+        if (char === str[i]) return i;
+    }
+    return -1;
 }
+
 let output = getIndexOf('a', 'I am a hacker');
 console.log(output);
