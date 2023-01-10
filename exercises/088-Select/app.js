ae3a8423b513ec15271369076634a06df2c4fa07
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-10 10:41:30 +0000

    exercise 88 passed

--- exercises/088-Select/app.js
+++ exercises/088-Select/app.js
@@ -1,1 +1,22 @@
-// Write your function here
+let arr = ['a', 'c', 'e'];
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+};
+
+function select(arr, obj) {
+    let myObj = {};
+    for (let key in obj) {
+        for (x of arr) {
+            if (x === key) {
+                myObj[key] = obj[key];
+            }
+        }
+    }
+    return myObj;
+}
+
+let output = select(arr, obj);
+console.log(output);
