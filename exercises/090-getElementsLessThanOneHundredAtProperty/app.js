66a9806e86c5a1a20bc0eea401b4139cb6c31f9d
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-11 11:14:37 +0000

    exercise 90 in progress

--- exercises/090-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/090-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,26 @@
-// Write your function here
+let obj = {
+    key: [10, 20, 50, 50]
+};
+
+function getElementsLessThan100AtProperty(obj, key) {
+    let myArr = [];
+
+    for (key in obj) {
+        if (!(Array.isArray(obj[key])) || obj[key] < 1) return [];
+        else
+            for (let i = 0; i < obj[key].length; i++) {
+                for (x of obj[key]) {
+                    if ((obj[key][i] && obj[key][i + 1]) > 100) return [];
+                    else if (x < 100) {
+                        myArr.push(x);
+                        i++;
+                    }
+
+                }
+            }
+    }
+    return myArr;
+}
+
+let output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output);
