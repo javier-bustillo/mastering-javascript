2d517d81ce8c0ec057a916ea74e1eb0c4ed8ae85
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-11 10:06:30 +0000

    exercise 89 passed

--- exercises/089-getElementsThatEqual10AtProperty/app.js
+++ exercises/089-getElementsThatEqual10AtProperty/app.js
@@ -1,1 +1,21 @@
-// Write your function here
+let obj = {
+    key: [10, 10, 10, 10]
+}
+
+function getElementsThatEqual10AtProperty(obj, key) {
+    let myArr = [];
+    for (key in obj) {
+        if (obj[key] < 1 || !(Array.isArray(obj[key]))) return [];
+        else
+            for (x of obj[key]) {
+                if (x !== 10) return [];
+                else if (x === 10) {
+                    myArr.push(x);
+                }
+            }
+    }
+    return myArr;
+}
+
+let output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output);
