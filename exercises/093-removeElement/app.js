0ef91cdacd21359746a87f4bb47bf5fe7bf585af
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-12 12:10:42 +0000

    exercise 93 passed

--- exercises/093-removeElement/app.js
+++ exercises/093-removeElement/app.js
@@ -1,1 +1,17 @@
-// Write your function here
+function removeElement(arr, n) {
+    let myArr = [];
+
+    if (arr.length < 1) return myArr;
+    else
+        for (x of arr) {
+            if (arr.every(x => x === n)) return myArr;
+            else if (!(arr.includes(n))) return 'discarder parameter is not included';
+            else if (x !== n) myArr.push(x);
+
+
+        }
+    return myArr;
+}
+
+let output = removeElement([1, 2, 3, 2, 1], 3);
+console.log(output);
