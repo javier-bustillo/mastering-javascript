ecefaa8f9350378977999fbdfe6307a1997dde43
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-12 11:41:24 +0000

    exercise 92 passed

--- exercises/092-getElementsGreaterThanTenAtProperty/app.js
+++ exercises/092-getElementsGreaterThanTenAtProperty/app.js
@@ -1,1 +1,17 @@
-// Write your function here
+let obj = {
+    key: [10, 2, 3]
+}
+
+function getElementsGreaterThan10AtProperty(obj, key) {
+    let myArr = [];
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return myArr;
+    else
+        for (x of obj[key]) {
+            if (obj[key].every(x => x < 10)) return myArr;
+            else if (x > 10) myArr.push(x);
+        }
+    return myArr;
+}
+
+let output = getElementsGreaterThan10AtProperty(obj, 'key');
+console.log(output);
