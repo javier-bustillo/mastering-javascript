ef7a9d696484234086c9117848fbf7075005456c
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-12 11:30:49 +0000

    exercise 91 passed

--- exercises/091-countAllCharacters/app.js
+++ exercises/091-countAllCharacters/app.js
@@ -1,1 +1,17 @@
-// Write your function here
+function countAllCharacters(str) {
+    let myObj = {};
+
+    if (str.length < 1) return myObj;
+    for (x of str) {
+        if (myObj[x] === undefined) {
+            myObj[x] = 1;
+        } else {
+            myObj[x] += 1;
+        }
+    }
+
+    return myObj;
+}
+
+let output = countAllCharacters('banana');
+console.log(output);
