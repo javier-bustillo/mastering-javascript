b67a002093fd931c5a059862aa020346f52664ec
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-13 09:58:03 +0000

    exercise 94 passed

--- exercises/094-getFirstElementOfProperty/app.js
+++ exercises/094-getFirstElementOfProperty/app.js
@@ -1,1 +1,13 @@
-// Write your function here
+let obj = {
+    key: [1, 2, 4]
+};
+
+function getFirstElementOfProperty(obj, key) {
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return undefined;
+    for (let i = 0; i < obj[key].length; i++) {
+        if (i === 0) return obj[key][i];
+    }
+}
+
+let output = getFirstElementOfProperty(obj, 'key');
+console.log(output);
