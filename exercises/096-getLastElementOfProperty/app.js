f94b703bfa5a39523faa07c384abd5230b4db9b7
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-13 10:29:37 +0000

    exercise 96 passed

--- exercises/096-getLastElementOfProperty/app.js
+++ exercises/096-getLastElementOfProperty/app.js
@@ -1,1 +1,12 @@
-// Write your function here
+let obj = {
+    key: [7, 2, 5, 10, 22]
+};
+
+
+function getLastElementOfProperty(obj, key) {
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return undefined;
+    else return obj[key][obj[key].length - 1];
+}
+
+let output = getLastElementOfProperty(obj, 'key');
+console.log(output);
