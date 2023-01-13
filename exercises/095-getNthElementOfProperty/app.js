7df9c676ecd546d2bf5f25fc4938ae1f38ab28ff
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-13 10:14:25 +0000

    exercise 95 passed

--- exercises/095-getNthElementOfProperty/app.js
+++ exercises/095-getNthElementOfProperty/app.js
@@ -1,1 +1,16 @@
-// Write your function here
+let obj = {
+    key: [1, 2, 6]
+};
+
+function getNthElementOfProperty(obj, key, n) {
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return undefined;
+
+    for (let i = 0; i < obj[key].length; i++) {
+        if (obj[key].length < n) return undefined;
+        else if (i === n) return obj[key][i];
+    }
+
+}
+
+let output = getNthElementOfProperty(obj, 'key', 2);
+console.log(output);
