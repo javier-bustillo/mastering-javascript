e773db3a95f55d8e9cee836495fa8e065bf98938
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 18:39:44 +0000

    exercise 106 passed

--- exercises/106-getEvenElementsAtProperty/app.js
+++ exercises/106-getEvenElementsAtProperty/app.js
@@ -1,10 +1,17 @@
-
 let obj = {
-  key: [1000, 11, 50, 17]
+    key: [1000, 11, 50, 17]
 };
 
 function getEvenElementsAtProperty(obj, key) {
-    // your code here
+    let myArr = [];
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return myArr;
+    else
+        for (x of obj[key]) {
+            if (obj[key].every(x => x % 2 !== 0)) return myArr;
+            myArr = obj[key].filter(x => x % 2 === 0);
+        }
+    return myArr;
 }
 let output = getEvenElementsAtProperty(obj, 'key');
 console.log(output); // --> [1000, 50]
