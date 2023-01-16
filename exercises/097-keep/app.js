99658ff1d7b7834e1dd244634a36c2ef84cdd4f3
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 17:12:42 +0000

    exercise 97 passed

--- exercises/097-keep/app.js
+++ exercises/097-keep/app.js
@@ -1,6 +1,11 @@
 function keep(arr, element) {
-
+    let myArr = [];
+    for (x of arr) {
+        if (arr.every(x => x !== element)) return myArr;
+        else if (x === element) myArr.push(x);
+    }
+    return myArr;
 }
 
-let output = keep([1, 2, 3, 2, 1], 2);
+let output = keep([1, 1, 3, 1, 1], 2);
 console.log(output);
