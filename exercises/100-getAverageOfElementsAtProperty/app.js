36e5e484c5906dafcf274631a57d323eb501d6b9
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 17:39:20 +0000

    exercise 100  passed

--- exercises/100-getAverageOfElementsAtProperty/app.js
+++ exercises/100-getAverageOfElementsAtProperty/app.js
@@ -1,4 +1,16 @@
-function getAverageOfElementsAtProperty(obj, key) {
-  // your code here
-
+let obj = {
+    key: [1, 2, 3]
 }
+
+function getAverageOfElementsAtProperty(obj, key) {
+    let aux = 0;
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return 0;
+    else
+        for (x of obj[key]) {
+            aux += x;
+        }
+    let averageOfNumbers = aux / obj[key].length;
+    return averageOfNumbers;
+}
+let output = getAverageOfElementsAtProperty(obj, 'key');
+console.log(output);
