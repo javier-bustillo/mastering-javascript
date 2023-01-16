b281ebb321329f98570e4dfc51e82fe48fbc43ff
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 17:24:19 +0000

    exercise 98 passed

--- exercises/098-getOddLengthWordsAtProperty/app.js
+++ exercises/098-getOddLengthWordsAtProperty/app.js
@@ -1,1 +1,16 @@
-// Write your function here
+let obj = {
+    key: ['It', 'hasi', 'some', 'wordsi']
+}
+
+function getOddLengthWordsAtProperty(obj, key) {
+    let myArr = [];
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return myArr;
+    else
+        for (x of obj[key]) {
+            if (obj[key].every(x => x % 2 === 0)) return myArr;
+            else if (x.length % 2 !== 0) myArr.push(x);
+        }
+    return myArr;
+}
+let output = getOddLengthWordsAtProperty(obj, 'key');
+console.log(output);
