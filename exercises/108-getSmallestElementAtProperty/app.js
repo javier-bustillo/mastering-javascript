e642636e2c9f2c4870aaa304cc8ad0e0db4243db
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 18:56:06 +0000

    exercise 108 passed

--- exercises/108-getSmallestElementAtProperty/app.js
+++ exercises/108-getSmallestElementAtProperty/app.js
@@ -1,8 +1,20 @@
 let obj = {
-  key: [2, 1, 5]
+    key: [2, 3, 1]
 };
+
 function getSmallestElementAtProperty(obj, key) {
-    // your code here
+
+    let aux = 999999999999;
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return [];
+    else
+        for (x of obj[key]) {
+            if (x < aux) aux = x;
+        }
+
+    return aux;
+
 }
+
 let output = getSmallestElementAtProperty(obj, 'key');
 console.log(output); // --> 1
