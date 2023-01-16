ad8e61fa0d7a3d7cbcac94e4ecd9fd40de16fdb2
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 18:15:47 +0000

    exercise 103 passed

--- exercises/103-getSquaredElementsAtProperty/app.js
+++ exercises/103-getSquaredElementsAtProperty/app.js
@@ -1,10 +1,19 @@
 let obj = {
-  key: [2, 1, 5]
+    key: [2, 1, 5]
 };
 
 function getSquaredElementsAtProperty(obj, key) {
-    // your code here
+    let myArr = [];
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return myArr;
+    else obj[key].forEach(element => {
+        myArr.push(Math.pow(element, 2))
+    });
+
+    return myArr;
+
 }
+
 
 let output = getSquaredElementsAtProperty(obj, 'key');
 console.log(output); // --> [4, 1, 25]
