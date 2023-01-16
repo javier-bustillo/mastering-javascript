94100120260fbe4c2f5c1ada11ade2c13c81ca5c
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 17:51:36 +0000

    exercise 101 passed

--- exercises/101-getEvenLengthWordsAtProperty/app.js
+++ exercises/101-getEvenLengthWordsAtProperty/app.js
@@ -1,8 +1,18 @@
 let obj = {
-  key: ['a', 'long', 'game']
+    key: ['a', 'longi', 'gamei']
 };
+
+
 function getEvenLengthWordsAtProperty(obj, key) {
-    // your code here
+    let myArr = [];
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return myArr;
+    else
+        for (x of obj[key]) {
+            if (obj[key].every(x => x.length % 2 !== 0)) return myArr;
+            else if (x.length % 2 === 0) myArr.push(x);
+        }
+    return myArr;
 }
 
 let output = getEvenLengthWordsAtProperty(obj, 'key');
