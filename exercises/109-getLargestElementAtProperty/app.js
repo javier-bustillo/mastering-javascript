42d44b924e11d9a9010df9dd44037b0a37058826
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 19:02:21 +0000

    exercise 109 passed

--- exercises/109-getLargestElementAtProperty/app.js
+++ exercises/109-getLargestElementAtProperty/app.js
@@ -1,8 +1,16 @@
 let obj = {
-  key: [1, 2, 4]
+    key: [1, 2, 4]
 };
+
 function getLargestElementAtProperty(obj, key) {
-    // your code here
+    let aux = 0;
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return [];
+    else
+        for (x of obj[key]) {
+            if (x > aux) aux = x;
+        }
+    return aux;
 }
 let output = getLargestElementAtProperty(obj, 'key');
 console.log(output); // --> 4
