716584bb97af80e76452eaf571d4cc8b1c1bef12
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 18:46:06 +0000

    exercise 107 passed

--- exercises/107-getLengthOfLongestElement/app.js
+++ exercises/107-getLengthOfLongestElement/app.js
@@ -1,5 +1,11 @@
 function getLengthOfLongestElement(arr) {
-    // Your code here
+    let aux = 0;
+    if (arr.length < 1) return 0;
+    else
+        for (x of arr) {
+            if (x.length > aux) aux = x.length;
+        }
+    return aux;
 }
 
 let output = getLengthOfLongestElement(['one', 'two', 'three']);
