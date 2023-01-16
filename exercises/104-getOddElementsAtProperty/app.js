107b067ad3c20cd35f1f1c2dd6e1ac2c62188438
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-16 18:21:47 +0000

    exercise 104 passed

--- exercises/104-getOddElementsAtProperty/app.js
+++ exercises/104-getOddElementsAtProperty/app.js
@@ -1,9 +1,17 @@
 let obj = {
-  key: [1, 2, 3, 4, 5]
+    key: [1, 2, 3, 4, 5]
 };
 
 function getOddElementsAtProperty(obj, key) {
-    // your code here
+    let myArr = [];
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return myArr;
+    else
+        for (x of obj[key]) {
+            if (obj[key].every(x => x % 2 === 0)) return myArr;
+            else if (x % 2 !== 0) myArr.push(x);
+        }
+    return myArr;
 }
 
 let output = getOddElementsAtProperty(obj, 'key');
