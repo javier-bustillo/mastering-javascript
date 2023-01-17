f4096935a029f499b7f7d6edcfc82fda099310d4
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-17 18:26:24 +0000

    exercise 111 passed

--- exercises/111-getElementOfArrayProperty/app.js
+++ exercises/111-getElementOfArrayProperty/app.js
@@ -3,6 +3,12 @@ let obj = {
 };
 
 function getElementOfArrayProperty(obj, key, index) {
-    // your code here
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return undefined;
 
+    for (let i = 0; i < obj[key].length; i++) {
+        if (i !== index) return undefined;
+        else if (i === index) return obj[key][i];
+    }
 }
+let output = getElementOfArrayProperty(obj, 'key', 2);
+console.log(output);
