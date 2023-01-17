11ffbc97629bcff728a2859cd1fb5d338d8fd809
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-17 18:33:28 +0000

    exercise 112 passed

--- exercises/112-squareElements/app.js
+++ exercises/112-squareElements/app.js
@@ -1,5 +1,10 @@
 function squareElements(arr) {
-  // your code here
+    let myArr = [];
+    arr.forEach(element => {
+        myArr.push(Math.pow(element, 2));
+    });
+
+    return myArr;
 }
 
 let output = squareElements([1, 2, 3]);
