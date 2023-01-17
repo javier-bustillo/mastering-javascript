00abdeb91994a2cff8b77ce3f86aeeac3e02c8d7
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-17 18:54:15 +0000

    exercise 114 passed

--- exercises/114-computeProductOfAllElements/app.js
+++ exercises/114-computeProductOfAllElements/app.js
@@ -1,7 +1,12 @@
 function computeProductOfAllElements(arr) {
-  // your code here
+    let aux = 1;
+    if (arr.length < 1) return 0;
+    else
+        for (let i = 0; i < arr.length; i++) {
+            aux *= arr[i];
+        }
+    return aux;
 }
 
 let output = computeProductOfAllElements([2, 5, 6]);
 console.log(output); // --> 60
-
