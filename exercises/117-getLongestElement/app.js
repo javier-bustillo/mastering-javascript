27feec72b60e9d8ed189e234a78496bd66e74a6c
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-21 12:59:41 +0000

    exercise 117 passed

--- exercises/117-getLongestElement/app.js
+++ exercises/117-getLongestElement/app.js
@@ -1,6 +1,17 @@
+let longestElement = '';
+
+
 function getLongestElement(arr) {
-    // your code here
-    
+    if (arr.length < 1) return '';
+
+    let elementsLength = arr.map(element => element.length);
+    let elementsSortedByLength = elementsLength.sort();
+    let longest = elementsSortedByLength[elementsSortedByLength.length - 1];
+
+    longestElement = arr.find(x => x.length === longest);
+
+    return longestElement;
+
 }
-let output = getLongestElement(['one', 'two', 'three']);
+let output = getLongestElement(['one', 'two', 'a', 'three', 'threes']);
 console.log(output); // --> 'three'
