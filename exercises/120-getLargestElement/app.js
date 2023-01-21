2969344d4203f2b5a39aedf3bac7ad2acb1c7a2e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-21 13:44:35 +0000

    exercise 120 passed

--- exercises/120-getLargestElement/app.js
+++ exercises/120-getLargestElement/app.js
@@ -1,7 +1,14 @@
+let aux = 0;
+
 function getLargestElement(arr) {
-  // your code here
-  
+    if (arr.length < 1) return 0;
+
+    for (x of arr) {
+        if (x > aux) aux = x;
+    }
+    return aux;
 }
 
 let output = getLargestElement([5, 2, 8, 3]);
+console.log(output); // --> 8;([5, 2, 8, 3]);
 console.log(output); // --> 8;
