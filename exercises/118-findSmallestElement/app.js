28428978aaf8f45359cae3ee1ddf4cf36e1ad9e5
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-21 13:06:22 +0000

    exercise 118 passed

--- exercises/118-findSmallestElement/app.js
+++ exercises/118-findSmallestElement/app.js
@@ -1,6 +1,12 @@
-function findSmallestElement(arr) {
-    // your code here
+let aux = 99999999;
 
+function findSmallestElement(arr) {
+    if (arr.length < 1) return 0;
+
+    for (x of arr) {
+        if (x < aux) aux = x;
+    }
+    return aux;
 }
 
 let output = findSmallestElement([4, 1, 9, 10]);
