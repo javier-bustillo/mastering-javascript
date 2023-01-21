59314b55d5e8468191a596dbe8993a114edb9c6e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-21 13:40:02 +0000

    exercise 119 passed

--- exercises/119-findShortestElement/app.js
+++ exercises/119-findShortestElement/app.js
@@ -1,7 +1,16 @@
 function findShortestElement(arr) {
-    // your code here
+    if (arr.length < 1) return '';
+
+    let elementsLength = arr.map(x => x.length);
+    let elementsSortedByLength = elementsLength.sort();
+    let shortest = elementsSortedByLength[0];
+
+    let shortestElement = arr.find(element =>
+        element.length === shortest);
+
+    return shortestElement;
 
 }
 
-let output = findShortestElement(['a', 'two', 'three']);
+let output = findShortestElement(['b', 'a', 'two', 'fifteen', 'three']);
 console.log(output); // --> 'a'
