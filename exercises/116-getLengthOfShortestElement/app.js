697af983481742ca38e71025305d4332b0f2161e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-21 11:49:24 +0000

    exercise 116 passed

--- exercises/116-getLengthOfShortestElement/app.js
+++ exercises/116-getLengthOfShortestElement/app.js
@@ -1,6 +1,13 @@
+let aux = 999999;
+
 function getLengthOfShortestElement(arr) {
-    // your code here
-    
+    if (arr.length < 1) return 0;
+    else
+        for (x of arr) {
+            if (x.length < aux) aux = x.length;
+        }
+    return aux;
 }
-let output = getLengthOfShortestElement(['one', 'two', 'three']);
+let output = getLengthOfShortestElement(['ones', 'two', 'three']);
+console.log(output); // --> 3estElement(['one', 'two', 'three']);
 console.log(output); // --> 3
