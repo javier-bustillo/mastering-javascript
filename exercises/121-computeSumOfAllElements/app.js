43de2af351b504de2b42bd92d2c607b363fe4075
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-21 13:47:42 +0000

    exercise 121 passed

--- exercises/121-computeSumOfAllElements/app.js
+++ exercises/121-computeSumOfAllElements/app.js
@@ -1,6 +1,11 @@
+let aux = 0;
+
 function computeSumOfAllElements(arr) {
-  // your code here
+    for (x of arr) {
+        aux += x;
+    }
+    return aux;
 }
 
-let output = computeSumOfAllElements([1, 2, 3])
+let output = computeSumOfAllElements([5, 3, 9])
 console.log(output); // --> 6
