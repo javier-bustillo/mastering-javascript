a7fe0d42d94d4e8f307ecab4abd754c0c0f8cd98
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-22 13:30:09 +0000

    exercise 128 passed

--- exercises/128-findShortestWordAmongMixedElements/app.js
+++ exercises/128-findShortestWordAmongMixedElements/app.js
@@ -1,5 +1,24 @@
 function findShortestWordAmongMixedElements(arr) {
-    // your code here
+    let shortestStrElement = '';
+    let aux = 9999999;
+    let myArr = [];
+
+    if (arr.length < 1) return '';
+    else
+        for (x of arr) {
+            if (arr.every(x => typeof x !== 'string')) return '';
+            else if (typeof x === 'string') {
+                let elementLength = arr.map(element => element.length);
+                let elementsSortedByLength = elementLength.sort();
+                let shortest = elementsSortedByLength[0];
+
+                let shortestElement = arr.find(element =>
+                    element.length === shortest);
+
+                return shortestElement;
+            }
+        }
 }
-let output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
+
+let output = findShortestWordAmongMixedElements(['one', 4, 'two ', 2, 'three ']);
 console.log(output); // --> 'two'
