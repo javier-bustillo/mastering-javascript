adbf00ab14e6784da55df0b9b594c2ac494734dc
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-22 10:46:25 +0000

    exercise 124 passed

--- exercises/124-joinArraysOfArrays/app.js
+++ exercises/124-joinArraysOfArrays/app.js
@@ -1,5 +1,14 @@
 function joinArrayOfArrays(arr) {
-  // your code here
+    let myArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        myArr = myArr.concat(arr[i]);
+    }
+    return myArr;
 }
-let output = joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']]);
+let output = joinArrayOfArrays([
+    [1, 4],
+    [true, false],
+    ['x', 'y']
+]);
 console.log(output); // --> [1, 4, true, false, 'x', 'y']
