9cd49d2dd94d1fbb791a7265dbf662029a757411
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-22 12:14:25 +0000

    exercise 127 passed

--- exercises/127-getSumOfAllElementsAtProperty/app.js
+++ exercises/127-getSumOfAllElementsAtProperty/app.js
@@ -3,7 +3,14 @@ let obj = {
 };
 
 function getSumOfAllElementsAtProperty(obj, key) {
-    // your code here
+    let initialValue = 0;
+
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return 0;
+
+    const sumOfAllElementsAtProperty = obj[key].reduce((accumulator, currentValue) =>
+        accumulator + currentValue, initialValue);
+
+    return sumOfAllElementsAtProperty;
 }
 let output = getSumOfAllElementsAtProperty(obj, 'key');
 console.log(output); // --> 13
