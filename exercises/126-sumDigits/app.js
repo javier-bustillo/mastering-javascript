be6f0ffa05592e1fc15905c9e5194079aaf920b0
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-22 12:03:49 +0000

    exercise 126 passed

--- exercises/126-sumDigits/app.js
+++ exercises/126-sumDigits/app.js
@@ -1,5 +1,22 @@
 function sumDigits(num) {
-    // your code here
+    let aux = 0;
+    let numToString = num.toString();
+
+    if (numToString[0] === '-') {
+        aux = aux - numToString[1];
+
+        for (let i = 2; i < numToString.length; i++) {
+            let str = Number(numToString[i]);
+            aux += str;
+        }
+    } else {
+        for (let i = 0; i < numToString.length; i++) {
+            let str = Number(numToString[i]);
+            aux += str;
+        }
+    }
+    return aux;
 }
+
 let output = sumDigits(316);
 console.log(output); // --> 4
