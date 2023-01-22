a13aabc30eb24a7a847fdabdef617dc699f74ffb
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-22 10:51:46 +0000

    exercise 125 passed

--- exercises/125-getProductOfAllElementsAtProperty/app.js
+++ exercises/125-getProductOfAllElementsAtProperty/app.js
@@ -1,9 +1,16 @@
 let obj = {
-  key: [1, 2, 3, 4]
+    key: [1, 2, 3, 4]
 };
 
+let aux = 1;
+
 function getProductOfAllElementsAtProperty(obj, key) {
-    // your code here
+    if (obj[key] < 1 || !(Array.isArray(obj[key]))) return 0;
+
+    for (x of obj[key]) {
+        aux *= x;
+    }
+    return aux;
 }
 let output = getProductOfAllElementsAtProperty(obj, 'key');
 console.log(output); // --> 24
