5676b94020497ab9951b48451d87ba58580796ae
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-23 09:42:04 +0000

    exercise 131 passed

--- exercises/131-getLargestNumberAmongMixedElements/app.js
+++ exercises/131-getLargestNumberAmongMixedElements/app.js
@@ -1,5 +1,16 @@
 function getLargestNumberAmongMixedElements(arr) {
-    // your code here
+    let myArr = [];
+
+    if (arr.length < 1) return 0;
+    if (arr.some(x => x === Number(x))) {
+        for (x of arr) {
+            if (x === Number(x))
+                myArr.push(x);
+        }
+    } else return 0;
+    let sorted = myArr.sort();
+    let largestNumber = sorted[sorted.length - 1];
+    return largestNumber;
 }
 
 let output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
