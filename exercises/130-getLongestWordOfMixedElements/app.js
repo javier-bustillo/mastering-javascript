707494a54fc12cc308e86afae1978b000b4f19ac
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-23 09:25:43 +0000

    exercise 130 passed

--- exercises/130-getLongestWordOfMixedElements/app.js
+++ exercises/130-getLongestWordOfMixedElements/app.js
@@ -1,6 +1,23 @@
 function getLongestWordOfMixedElements(arr) {
-    // your code here
-}
+    let myArr = [];
 
+    if (arr.length < 1) return '';
+    for (x of arr) {
+        if (arr.some(x => x === String(x))) {
+            if (x === String(x)) {
+                myArr.push(x);
+            }
+        } else {
+            return '';
+        }
+    }
+    let elementsLength = myArr.map(element => element.length);
+    let elementsSortedByLength = elementsLength.sort();
+    let longest = elementsSortedByLength[elementsSortedByLength.length - 1];
+
+    longestElement = arr.find(x => x.length === longest);
+    return longestElement;
+
+}
 let output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
 console.log(output); // --> 'word'
