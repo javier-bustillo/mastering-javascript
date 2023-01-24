184318d95414f9d18a28f2bc04d47a6c0ef27c4e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-24 11:07:34 +0000

    exercise 135 passed

--- exercises/135-computeFactorialOfN/app.js
+++ exercises/135-computeFactorialOfN/app.js
@@ -1,5 +1,20 @@
+/* function computeFactorialOfN(n) {
+    if (n < 0) return 'Number has to be positive';
+    if (n === 0 || n === 1) return 1;
+    else return n = n * computeFactorialOfN(n - 1);
+} */
+
 function computeFactorialOfN(n) {
-    // your code here
+    let result = 1;
+
+    if (n < 0) return 'Number has to be positive';
+    if (n === 0 || n === 1) return 1;
+    else
+        for (let i = n; i >= 1; i--) {
+            result = result * i;
+        }
+    return result;
 }
 
-let output = computeFactorialOfN(3); console.log(output); // --> 6
+let output = computeFactorialOfN(4);
+console.log(output); // --> 6
