fe42f977f851172310b0c3b22ae440217c01d333
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-25 11:12:53 +0000

    exercise 137 passed

--- exercises/137-getLongestOfThreeWords/app.js
+++ exercises/137-getLongestOfThreeWords/app.js
@@ -1,5 +1,20 @@
 function getLongestOfThreeWords(word1, word2, word3) {
-    // your code here
+    let myArr = [];
+
+    myArr.push(word1);
+    myArr.push(word2);
+    myArr.push(word3);
+
+    let elementsLength = myArr.map(element => element.length);
+    let elementsSortedByLength = elementsLength.sort();
+    let longest = elementsSortedByLength[elementsSortedByLength.length - 1];
+
+    longestElement = myArr.find(x => x.length === longest);
+
+    return longestElement;
+
+
+
 }
 
 let output = getLongestOfThreeWords('these', 'three', 'words');
--- exercises/137-getLongestOfThreeWords/solution.hide.js
+++ exercises/137-getLongestOfThreeWords/solution.hide.js
@@ -1,11 +1,11 @@
 function getLongestOfThreeWords(word1, word2, word3) {
-  // your code here
-  let aux = '';
-  if (word1.length > aux.length) aux = word1;
-  if (word2.length > aux.length) aux = word2;
-  if (word3.length > aux.length) aux = word3;
-  return aux;
+    // your code here
+    let aux = '';
+    if (word1.length > aux.length) aux = word1;
+    if (word2.length > aux.length) aux = word2;
+    if (word3.length > aux.length) aux = word3;
+    return aux;
 }
 
-var output = getLongestOfThreeWords('these', 'three', 'words');
+var output = getLongestOfThreeWords('these', 'threes', 'words');
 console.log(output); // --> 'these'
