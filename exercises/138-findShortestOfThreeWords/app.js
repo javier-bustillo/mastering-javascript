b4e67413b03613e14403217111bb51062515004b
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-25 11:27:53 +0000

    exercise 138 passed

--- exercises/138-findShortestOfThreeWords/app.js
+++ exercises/138-findShortestOfThreeWords/app.js
@@ -5,13 +5,14 @@ function findShortestOfThreeWords(word1, word2, word3) {
     myArr.push(word2);
     myArr.push(word3);
 
+
     let elementsLength = myArr.map(element => element.length);
     let elementsSortedByLength = elementsLength.sort();
     let shortest = elementsSortedByLength[0];
 
-    shortestElement = myArr.find(x => x.length === shortest);
+    let shortestElement = myArr.find(x => x.length === shortest);
 
     return shortestElement;
 }
-let output = findShortestOfThreeWords('a', 'two', 'three', 'b');
+let output = findShortestOfThreeWords('a', 'two', 'three');
 console.log(output); // --> 'a'
