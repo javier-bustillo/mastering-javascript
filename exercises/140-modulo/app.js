90e0197c90ab06eb0aaf75856b56c0e0dec50d39
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-27 11:03:22 +0000

    exercise 140 in progress

--- exercises/140-modulo/app.js
+++ exercises/140-modulo/app.js
@@ -1,12 +1,6 @@
 function modulo(num1, num2) {
-    let i = 0;
-    let modulo = 0;
-
-    for (i; i < num1; i++) {
-        i += num1 - i;
-    }
-    console.log(i);
+    // your code here
 }
 
 let output = modulo(25, 4);
-console.log()
+console.log(output); // --> 1
