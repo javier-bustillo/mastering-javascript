ef8a77e030fdb1738490c946389ad04e62ce1bb7
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-29 19:43:33 +0000

    exercise 141 passed

--- exercises/141-multiply/app.js
+++ exercises/141-multiply/app.js
@@ -1,6 +1,12 @@
 function multiply(num1, num2) {
-    // your code here
+    let result = 0;
+    if (num1 < 0) {
+        for (let i = 0; i < num2; i++) result += num1;
+    } else {
+        for (let i = 0; i < num1; i++) result += num2;
+    }
+    return result;
 }
 
-let output = multiply(4, 7);
+let output = multiply(-3, 7);
 console.log(output); // --> 28
