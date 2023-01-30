620fabdb42c83b69b93f20353392446cf02c7dc4
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-30 08:49:20 +0000

    exercise 142 passed

--- exercises/142-isOddWithoutModulo/app.js
+++ exercises/142-isOddWithoutModulo/app.js
@@ -1,5 +1,12 @@
 function isOddWithoutModulo(num) {
-    // your code here
+    let x = num / 2;
+    let y = Math.trunc(x);
+    let z = y * 2;
+    let result = num - z;
+
+    if (result !== 0) return true;
+    else return false;
+
 }
 let output = isOddWithoutModulo(17);
 console.log(output); // --> true
