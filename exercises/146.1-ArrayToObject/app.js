1469925924ff6cf48020159664ab7b174736892a
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-01 08:29:52 +0000

    exercise 146.1 passed

--- exercises/146.1-ArrayToObject/app.js
+++ exercises/146.1-ArrayToObject/app.js
@@ -1,3 +1,12 @@
+let obj = {};
+
 function transformFirstAndLast(array) {
-  // your code here
+    let firstElement = array[0];
+    let lastElement = array[array.length - 1];
+    obj[firstElement] = lastElement;
+
+    return obj;
 }
+
+let output = transformFirstAndLast(['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey']);
+console.log(output);
