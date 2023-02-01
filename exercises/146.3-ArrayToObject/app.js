91d25e6e09aa642fe72c99e9b0bb946489b4b63b
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-01 09:27:50 +0000

    exercise 146.3 passed

--- exercises/146.3-ArrayToObject/app.js
+++ exercises/146.3-ArrayToObject/app.js
@@ -1,3 +1,26 @@
 function transformEmployeeData(array) {
-  // your code here
+    let myArr = [];
+    for (let x of array) {
+        let obj = {};
+        for (let y of x) {
+            obj[y[0]] = y[1];
+        }
+        myArr.push(obj);
+    }
+    return myArr;
 }
+let output = transformEmployeeData([
+    [
+        ['firstName', 'Joe'],
+        ['lastName', 'Blow'],
+        ['age', 42],
+        ['role', 'clerk']
+    ],
+    [
+        ['firstName', 'Mary'],
+        ['lastName', 'Jenkins'],
+        ['age', 36],
+        ['role', 'manager']
+    ]
+]);
+console.log(output);
