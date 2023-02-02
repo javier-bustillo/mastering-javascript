3297466b745134bc177fdf659de9dc92414970ba
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-02 09:19:00 +0000

    exercises 146.2 improved

--- exercises/146.2-ArrayToObject/app.js
+++ exercises/146.2-ArrayToObject/app.js
@@ -1,12 +1,17 @@
 let obj = {};
 
 function fromListToObject(array) {
+    /* for (x of array) {
+            obj[x[0]] = x[1];
+        }
 
-    for (x of array) {
-        obj[x[0]] = x[1];
-    }
+        return obj; */
+
+    const entries = new Map(array);
+    const obj = Object.fromEntries(entries);
 
     return obj;
+
 }
 
 let output = fromListToObject([
