b78a811b02bde38aaee1a8b7b7c2442b25d43429
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-06 18:57:01 +0000

    exercise 151 passed

--- exercises/151-detectOutlierValue/app.js
+++ exercises/151-detectOutlierValue/app.js
@@ -1,11 +1,28 @@
 function detectOutlierValue(string) {
+    let oddNumbers = 0;
+    let evenNumbers = 0;
+    let strToArr = string.split(' ');
 
-
+    for (x of strToArr) {
+        if (Number.parseInt(x) % 2 !== 0) oddNumbers += 1;
+        if (Number.parseInt(x) % 2 === 0) evenNumbers += 1;
+    }
+    if (evenNumbers > oddNumbers) {
+        for (x of strToArr) {
+            if (Number.parseInt(x) % 2 !== 0) return ((strToArr.indexOf(x) + 1));
+        }
+    } else {
+        for (x of strToArr) {
+            if (Number.parseInt(x) % 2 === 0) return ((strToArr.indexOf(x) + 1));
+        }
+    }
 
 }
 
 // Third number is odd, while the rest of the numbers are even
-console.log(detectOutlierValue("2 4 7 8 10")); // => 3 
+let output = detectOutlierValue("2 4 7 8 10");
+console.log(output); // 3
 
 // Second number is even, while the rest of the numbers are odd
-// console.log(detectOutlierValue("1 10 1 1")) //=> 2
+let output1 = detectOutlierValue("1 10 1 1");
+console.log(output1); // 2
