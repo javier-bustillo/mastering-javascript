c182c484f282284c58550f3c7d0aff0ab3696d4e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-07 17:50:55 +0000

    exercise 152 passed

--- exercises/152-findPairForSum/app.js
+++ exercises/152-findPairForSum/app.js
@@ -1,5 +1,12 @@
-function findPairForSum(array, number){
-  
+function findPairForSum(array, number) {
+    let result = [];
+    for (x of array) {
+        for (y of array) {
+            if (x + y === number) {
+                return result = [x, y];
+            }
+        }
+    }
 }
 
 let pair = findPairForSum([3, 34, 4, 12, 5, 2], 9);
