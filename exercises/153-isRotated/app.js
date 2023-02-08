1272cceed491e68af2ed116c85fb0971a2332d60
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-08 18:41:16 +0000

    exercise 153 passed

--- exercises/153-isRotated/app.js
+++ exercises/153-isRotated/app.js
@@ -1,6 +1,15 @@
 function isRotated(str1, str2) {
-    // your code here
+    let fracc = str1.split('');
+    let aux = [];
+    let result;
+
+    for (let i of fracc) {
+        aux.push(str2.indexOf(i));
+    }
+    result = !aux.includes(-1);
+    return result;
 }
 
-console.log(isRotated('hello world', 'orldhello w')) // => true
+// console.log(isRotated('hello world', 'orldhello w')) // => true
 console.log(isRotated('hello world', 'omrel wp')) // => false
+// console.log(isRotated('Work from Home', 'kfmoemro Hor W'));
