431ca1aa3e30ebca94703c3aaff779be303b3455
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-12 17:58:33 +0000

    exercise 156 passed

--- exercises/156-isogram/app.js
+++ exercises/156-isogram/app.js
@@ -1,9 +1,14 @@
 function isIsogram(text) {
-  // add each char to a set
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
-  // note: a set drops dup values
-  // thus, to see if all the chars were unique,
-  // check length of text and the size of the set 
+    // add each char to a set
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+    // note: a set drops dup values
+    // thus, to see if all the chars were unique,
+    // check length of text and the size of the set 
+    let charSet = new Set();
+    for (char of text) {
+        charSet.add(char.toLowerCase());
+    }
+    return charSet.size === text.length ? true : false;
 }
 
 console.log(isIsogram("Camile")) // => true
