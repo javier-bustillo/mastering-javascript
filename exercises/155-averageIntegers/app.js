b186fab71847a8a1631000ab0ae86321c2c3bb59
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-12 17:38:04 +0000

    exercise 155 code cleaner

--- exercises/155-averageIntegers/app.js
+++ exercises/155-averageIntegers/app.js
@@ -1,10 +1,13 @@
 function average(array_of_numbers) {
-    let averageResult = sum(array_of_numbers) / array_of_numbers.length;
+    let arrLength = array_of_numbers.length;
+    let averageResult = sum(array_of_numbers) / arrLength;
+
     return averageResult;
 }
 
 function sum(numbers) {
     let sumResult = numbers.reduce((a, b) => a + b, 0);
+
     return sumResult;
 }
 
