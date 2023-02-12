32d5de3d5286aac5a363406142d9faa0255b7b18
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-12 18:26:05 +0000

    exercise 157 in progress

--- exercises/157-phoneNumber/app.js
+++ exercises/157-phoneNumber/app.js
@@ -1,29 +1,32 @@
 function PhoneNumberFormatter(numbers) {
-  this.numbers = numbers;
+    this.numbers = numbers;
 }
 
 PhoneNumberFormatter.prototype.render = function() {
-  let string = '';
-  // your code here
-  return string;
+    let string = '';
+    // your code here
+    return string;
 };
 
 PhoneNumberFormatter.prototype.getAreaCode = function() {
-  // your code here
+    // your code here
 };
 
 PhoneNumberFormatter.prototype.getExchangeCode = function() {
-  // your code here
+    // your code here
 };
 
 PhoneNumberFormatter.prototype.getLineNumber = function() {
-  // your code here
+    // your code here
 };
 
 PhoneNumberFormatter.prototype.parenthesize = function(string) {
-  return '(' + string + ')';
+    return '(' + string + ')';
 };
 
 PhoneNumberFormatter.prototype.slice = function(start, end) {
-  return this.numbers.slice(start, end).join('');
+    return this.numbers.slice(start, end).join('');
 };
+
+const myArr = [6, 5, 0, 8, 3, 5, 9, 1, 7, 2];
+console.log(PhoneNumberFormatter(myArr));
