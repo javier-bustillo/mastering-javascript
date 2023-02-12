91fd9de3ab735c71cd3493734ae99b7585486aa5
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-12 17:36:11 +0000

    exercise 155 passed

--- exercises/155-averageIntegers/app.js
+++ exercises/155-averageIntegers/app.js
@@ -1,7 +1,12 @@
 function average(array_of_numbers) {
-  // process array of numbers
+    let averageResult = sum(array_of_numbers) / array_of_numbers.length;
+    return averageResult;
 }
 
 function sum(numbers) {
-    
+    let sumResult = numbers.reduce((a, b) => a + b, 0);
+    return sumResult;
 }
+
+console.log(sum([1, 2]));
+console.log(average([1, 2]));
