4bdfeb079ceedf0daa473c8f8f6286f0843c889e
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-13 19:22:02 +0000

    exercise 158 in progress

--- exercises/158-longestPalindrome/app.js
+++ exercises/158-longestPalindrome/app.js
@@ -1,14 +1,30 @@
 function findLongestPalindrome(sentence) {
-  // split sentence into words
-  // iterate words and collect the palindromes
-  // sort the list of palindromes by word length
-  // return the largest item in the sorted list
+    // split sentence into words
+    // iterate words and collect the palindromes
+    // sort the list of palindromes by word length
+    // return the largest item in the sorted list
+
+
 }
 
 function reverseString(string) {
-  
+    let strIntoWords = string.split(" ");
+    let reverseWords = [];
+    for (word of strIntoWords) {
+        reverseWords.push(word.split("").reverse().join(""));
+    }
+    return reverseWords;
 }
 
 function isPalindrome(word) {
-  // hint: you can detect palindromes by comparing a string to its reverse
+    // hint: you can detect palindromes by comparing a string to its reverse
+    for (word of reverseString()) {
+        console.log(word);
+        for ()
+    }
+
 }
+
+let output = findLongestPalindrome("My dad is a racecar athlete");
+console.log(output); // "a racecar a"
+console.log(reverseString("My dad is a racecar athlete"));
