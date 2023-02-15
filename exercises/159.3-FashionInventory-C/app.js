47ea978900915de2c74d3ebd41b02bb5e15c67a9
javier-bustillo <jjavier.bustillo@gmail.com>
2023-02-15 12:00:54 +0000

    exercise 159.3 passed

--- exercises/159.3-FashionInventory-C/app.js
+++ exercises/159.3-FashionInventory-C/app.js
@@ -1,22 +1,49 @@
-let inventory = [
-    {
-      name: 'Brunello Cucinelli',
-      shoes: [
-        {name: 'tasselled black low-top lace-up', price: 1000},
-        {name: 'tasselled green low-top lace-up', price: 1100},
-        {name: 'plain beige suede moccasin', price: 950},
-        {name: 'plain olive suede moccasin', price: 1050}
-      ]
+let inventory = [{
+        name: 'Brunello Cucinelli',
+        shoes: [{
+                name: 'tasselled black low-top lace-up',
+                price: 1000
+            },
+            {
+                name: 'tasselled green low-top lace-up',
+                price: 1100
+            },
+            {
+                name: 'plain beige suede moccasin',
+                price: 950
+            },
+            {
+                name: 'plain olive suede moccasin',
+                price: 1050
+            }
+        ]
     },
     {
-      name: 'Gucci',
-      shoes: [
-        {name: 'red leather laced sneakers', price: 800},
-        {name: 'black leather laced sneakers', price: 900}
-      ]
+        name: 'Gucci',
+        shoes: [{
+                name: 'red leather laced sneakers',
+                price: 800
+            },
+            {
+                name: 'black leather laced sneakers',
+                price: 900
+            }
+        ]
     }
-  ];
+];
 
 function renderInventory(shoeList) {
-  
+    let result = [];
+
+    shoeList.map(e => {
+        e.shoes.map(x => {
+            if (x.name.includes('black')) {
+                result.push([e.name, x.name, x.price]);
+            }
+        });
+    });
+    return result;
 }
+
+let output = renderInventory(inventory);
+console.log(output);
